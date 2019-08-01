b36f72bccdbe68ce877942b5cd709a6af289a694
Joonas Maliniemi <joonas.maliniemi@vr.fi>
2019-08-01 13:14:18 +0300

    Prune repo

--- types/koa-proxy/koa-proxy-tests.ts
+++
@@ -1,9 +0,0 @@
-import Koa = require('koa');
-import proxy = require('koa-proxy');
-
-const app = new Koa();
-app.use(proxy({
-    host: 'https://api.github.com',
-    match: /^\/repos\//
-}));
-app.listen(3000);
